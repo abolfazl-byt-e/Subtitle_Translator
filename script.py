f5c7e07d895ba58b1fceb8945559cc6a01bcd362
abolfazlbyte <abolfazl.byte@gmail.com>
2020-09-19 18:26:29 +0430

    add srt and vtt translator

--- script.py
+++ script.py
@@ -2,21 +2,61 @@ from googletrans import Translator
 import webvtt
 
 translator = Translator()
-# result = translator.translate("hello ,nodjs how are you.", dest="fa").text
-# result = result.encode('utf-8').decode('utf-8')
-# print(result)
 
-with open("./vtt/translated.vtt", "a+") as f:
-    f.write("WEBVTT\n\n")
+#TODO get the file
+file_name = input('FILE NAME: enter file name with extention  .vtt .srt .sbv\n')
+x = file_name[:-4]
+y = file_name[-4:]
+file_name_persian = x + "_persian" + y
+print(file_name)
+print(file_name_persian)
 
-    captions = webvtt.read("./vtt/original.vtt")
-    for caption in captions:
-        # print(caption.start,"-->", caption.end)
-        f.write(caption.start)
-        f.write(" --> ")
-        f.write(caption.end)
-        f.write("\n")
-        # print(translator.translate(caption.text, src="en", dest="fa").text)
-        f.write(translator.translate(caption.text, src="en", dest="fa").text)
-        f.write("\n\n")
-        # print()
+#TODO functions
+def vtt_translate(file_name, file_name_persian):
+    with open("./pase_it/%s"%file_name_persian, "a+") as f:
+        f.write("WEBVTT\n\n")
+
+        captions = webvtt.read("./pase_it/%s"%file_name)
+        for caption in captions:
+            # print(caption.start,"-->", caption.end)
+            f.write(caption.start)
+            f.write(" --> ")
+            f.write(caption.end)
+            f.write("\n")
+            # print(translator.translate(caption.text, src="en", dest="fa").text)
+            f.write(translator.translate(caption.text, src="en", dest="fa").text)
+            f.write("\n\n")
+            # print()
+        f.close()
+        print("Done. --> /pase_it/%s"%file_name_persian)
+
+def srt_translate(file_name, file_name_persian):
+    with open("./pase_it/%s"%file_name_persian, "a+") as f:
+
+        counter = 1
+        captions = webvtt.from_srt("./pase_it/%s"%file_name)
+        for caption in captions:
+            f.write(str(counter))
+            f.write("\n")
+            # print(caption.start,"-->", caption.end)
+            f.write(caption.start)
+            f.write(" --> ")
+            f.write(caption.end)
+            f.write("\n")
+            # print(translator.translate(caption.text, src="en", dest="fa").text)
+            f.write(translator.translate(caption.text, src="en", dest="fa").text)
+            f.write("\n\n")
+            # print()
+            counter +=1
+        f.close()
+        print("Done. --> /pase_it/%s"%file_name_persian)
+
+#TODO detect file format
+if y == ".srt":
+    srt_translate(file_name, file_name_persian)
+elif y == ".vtt":
+    vtt_translate(file_name, file_name_persian)
+elif y == ".sbv":
+    pass
+else:
+    print("dont detect!!!")
